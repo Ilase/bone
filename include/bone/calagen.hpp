b7f138b9742c0b6a85c6616b951b49d40bdd81cb
Ilase <ilya280505@gmail.com>
2024-08-23 16:52:06 +0500

    wtf no fckin namespace

--- include/bone/calagen.hpp
+++ include/bone/calagen.hpp
@@ -16,6 +16,15 @@
 namespace bone
 {
     /*
+    @brief Enum for dispaly or get connection status. Use 'static_cast<int>' for get variable of status!
+    */
+    enum connection_status {
+        connected,
+        disconected,
+        suspended,
+        in_progress,
+    };
+    /*
     @brief calagen error hendler
     */
     void calagen_error(const std::exception& e, const std::string& additional_info = "") {
@@ -24,20 +33,33 @@ namespace bone
         std::cerr << "Additional info: " << additional_info << std::endl;
         }
     }
+    /*
+    @brief Main class for connection 
+    */
     class calagen {
         std::string name;
         ssh_session session;
         std::string address;
         int verbocity;
         int port;
+        connection_status status;
 
         public:
+        /*
+        @brief Open new session 
+        */
         calagen(std::string name, std::string adress, int port);
         ~calagen();
         inline bool is_active();
-        void open();
+        //void open();
+
         void close();
+        //======================================================
+        //Getters and setters
+        void set_status(connection_status status);
         std::string get_adress();
+        connection_status get_status();
+
     };
 } // namespace calagen
 
